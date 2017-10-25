c7dff2910ba5343aa2a4cb85e6102af9a68465a7
Matt Marshall <matt@pixeltours.com>
2017-10-25 13:21:26 -0400

    - Yulio Capture with Scriptable Wizard - Added unitypackage for relase.

---
+++ src/Assets/YulioCapture/Editor/YulioCaptureWizard.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+namespace PTLabs
+{
+	public enum ImageFormat
+	{
+		PNG,
+		JPEG
+	}
+
+    public class YulioCaptureWizard : ScriptableWizard
+    {
+
+        public Camera targetCamera;
+        public float ipd = 0.063f;
+        public int faceResolution = 1536;
+        public ImageFormat imageFormat = ImageFormat.PNG;
+
+        static string yulioURL = "http://www.yulio.com";
+
+		[MenuItem("Tools/Yulio/Export Yulio Cubemaps")]
+		static void CreateWizard()
+		{
+            ScriptableWizard.DisplayWizard<YulioCaptureWizard>("Export Cubemaps", "Export", "Open Yulio");
+		}
+
+		[MenuItem("Tools/Yulio/Go to Yulio.com")]
+		static void OpenYulio()
+		{
+            Application.OpenURL(yulioURL);
+		}
+
+        // Validates that there is a camera to render from. 
+		void OnWizardUpdate()
+		{
+			helpString = "Select a camera to generate cubemaps from.";
+            if (targetCamera == null)
+			{
+				errorString = "Please assign an camera";
+				isValid = false;
+			}
+			else
+			{
+				errorString = "";
+				isValid = true;
+			}
+		}
+
+        // Add YulioCapture to the camera and generate the cubemap. 
+		void OnWizardCreate()
+		{
+            GameObject activeGameObject = targetCamera.gameObject;
+            YulioCapture captureComponent = activeGameObject.AddComponent<YulioCapture>();
+
+            // render the texture
+            Texture2D textureOutput = captureComponent.RenderCubemapTexture(faceResolution, ipd);
+
+            string fullPath = EditorUtility.SaveFilePanel(string.Format("Save Yulio Cubemap as {0}", imageFormat.ToString()), "", "Yulio Cubemap", imageFormat.ToString().ToLower());
+
+            // write to a nice Yulio-ready file. Yum.
+            switch(imageFormat)
+            {
+                case ImageFormat.PNG:
+                    File.WriteAllBytes(fullPath,textureOutput.EncodeToPNG());
+                    break;
+                case ImageFormat.JPEG:
+                    File.WriteAllBytes(fullPath, textureOutput.EncodeToJPG());
+                    break;
+                default:
+                    File.WriteAllBytes(fullPath, textureOutput.EncodeToJPG());
+                    break;
+            }
+
+            // remove the capture component from the camera, leaving it as it was.
+            DestroyImmediate(captureComponent);
+
+            // refresh asset database in case cubemap was saved to Unity Project. 
+            AssetDatabase.Refresh();
+		}
+
+        void OnWizardOtherButton()
+        {
+            Application.OpenURL(yulioURL);
+        }
+
+
+
+    }
+}
---
+++ src/Assets/YulioCapture/YulioCapture.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+namespace PTLabs
+{
+    [ExecuteInEditMode]
+    [RequireComponent(typeof(Camera))]
+    public class YulioCapture : MonoBehaviour
+    {
+        bool isRendering = false;
+
+        Camera captureCamera;
+
+        public Texture2D RenderCubemapTexture(int _faceResolution, float _ipd)
+        {
+            isRendering = true;
+            captureCamera = GetComponent<Camera>();
+            int x = 0;
+            int y = 0;
+
+            // order of the cubemap faces for Yulio intake. 
+            CubemapFace[] facesToRender = new CubemapFace[] {
+                CubemapFace.NegativeX, CubemapFace.PositiveZ,
+                CubemapFace.PositiveX, CubemapFace.NegativeZ,
+                CubemapFace.NegativeY, CubemapFace.PositiveY,
+            };
+
+            Cubemap leftEye = new Cubemap(_faceResolution, TextureFormat.RGB24, false);
+            Cubemap rightEye = new Cubemap(_faceResolution, TextureFormat.RGB24, false);
+
+            // create a texture for 6x2 cubemap faces
+            var textureOutput = new Texture2D(leftEye.width * 6, leftEye.height * 2, TextureFormat.RGB24, false);
+
+            // renders each eye to a cubemap. 
+            Vector3 startingPos = captureCamera.transform.position;
+            captureCamera.RenderToCubemap(leftEye);
+            captureCamera.transform.Translate((new Vector3(-_ipd, 0, 0)));
+            captureCamera.RenderToCubemap(rightEye);
+
+            // draw the right eye to the texture
+            for (int i = 0; i < 6; i++)
+            {
+                Color[] pixels = rightEye.GetPixels(facesToRender[i]);
+                Color[] sourcePixels = new Color[pixels.Length];
+                for (int ypos = 0; ypos < _faceResolution; ypos++)
+                {
+                    for (int xpos = 0; xpos < _faceResolution; xpos++)
+                    {
+                        sourcePixels[ypos * _faceResolution + xpos] = pixels[((_faceResolution - 1 - ypos) * _faceResolution) + xpos];
+                    }
+                }
+                // Copy them to the dest texture
+                textureOutput.SetPixels(x, y, _faceResolution, _faceResolution, sourcePixels);
+
+                // move to the next face
+                x += _faceResolution;
+            }
+
+            // reset x and move to the next row
+            x = 0;
+            y += _faceResolution;
+
+            // draw the left eye
+            for (int i = 6; i < 12; i++)
+            {
+                Color[] pixels = leftEye.GetPixels(facesToRender[i - 6]);
+                Color[] sourcePixels = new Color[pixels.Length];
+                for (int ypos = 0; ypos < _faceResolution; ypos++)
+                {
+                    for (int xpos = 0; xpos < _faceResolution; xpos++)
+                    {
+                        sourcePixels[ypos * _faceResolution + xpos] = pixels[((_faceResolution - 1 - ypos) * _faceResolution) + xpos];
+                    }
+                }
+                // Copy them to the dest texture
+                textureOutput.SetPixels(x, y, _faceResolution, _faceResolution, sourcePixels);
+
+                // move to the next face
+                x += _faceResolution;
+            }
+
+            DestroyImmediate((leftEye));
+            DestroyImmediate(rightEye);
+            isRendering = false;
+
+            return textureOutput;
+        }
+    }
+}
+
